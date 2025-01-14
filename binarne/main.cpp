828ea86236774cbdc8faf5f421c4641bba40db8d
memorieplastique <99959203+memorieplastique@users.noreply.github.com>
2025-01-14 16:57:07 +0100

    Update main.cpp

--- binarne/main.cpp
+++ binarne/main.cpp
@@ -27,8 +27,3 @@ int binToDec(int num) {
     }
 
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
